00aeb688d5c8f1a86deafa4001f67a7a314b6988
r3troseer <piagbo3d@gmail.com>
2022-08-14 22:38:30 +0100

    added more design to the homepage and corrected datetime field in models.py

--- base/models.py
+++ base/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from autoslug import AutoSlugField
+from datetime import date, time
 
 
 class Priority(models.Model):
@@ -27,8 +28,8 @@ class Task(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(max_length=500)
     slug = AutoSlugField(populate_from="name", null=True)
-    due_date = models.DateField
-    due_time = models.TimeField
+    due_date = models.DateField(("Date"), default=date.today)
+    due_time = models.TimeField(("Time"), default=time)
     priority = models.ForeignKey(Priority, on_delete=models.SET_NULL, null=True)
     repeat = models.ForeignKey(Repeat, on_delete=models.SET_NULL, null=True)
     tag = models.ForeignKey(Tag, on_delete=models.SET_NULL, null=True)
